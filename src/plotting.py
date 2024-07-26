38266687bdb27d67067f78c575a37398992526b8
Ali Vakilian <a.vakilian@gmail.com>
2024-07-26 10:34:34 -0500

    plot the new baseline (over validation set)

--- src/plotting.py
+++ src/plotting.py
@@ -18,12 +18,12 @@ def do_plotting(display_plots, save_plots, use_input_commands, numsteps, group_n
 
     # Create a list of all figures we want to save for later which will be passed into a function
 
-    str_learner ={True: 'strategic', False: 'non-strategic'}
+    str_learner ={True: 'S', False: 'NS'}
     
-    strategic_string = f'of {str_learner[strategic_learner]} learner'
+    strategic_string = f'of {str_learner[strategic_learner[0]]}-{str_learner[strategic_learner[1]]} learner'
 
     figures = []
-    learner_name = str_learner[strategic_learner][0].upper() + str_learner[strategic_learner][1:]
+    learner_name = f'{str_learner[strategic_learner[0]]}-{str_learner[strategic_learner[1]]} learner'
     figure_names = [f'{learner_name}_PopError_vs_Rounds', f'{learner_name}_GroupError_vs_Rounds', f'{learner_name}_GroupWeights_vs_Rounds', f'{learner_name}_Trajectory_Plot']
 
     # Combine all the existing arrays as necessary by separating all subgroups as unqiue groups
@@ -166,68 +166,132 @@ def do_plotting(display_plots, save_plots, use_input_commands, numsteps, group_n
         plt.close('all')
 
 
-def plot_write_overall(pop_error_type, dirname, data_name, max_error, avg_error, tau, display_plots = False):
+def plot_write_overall(pop_error_type, dirname, data_name, max_error, avg_error, val_max_error, val_avg_error, tau, display_plots = False):
     dataset_string = f' on {data_name[0].upper() + data_name[1:]}'  # Set the first letter to capital if it isn't
     tau_values = list(max_error.keys())
-    max_error_array = np.zeros((len(tau_values),2))
-    avg_error_array = np.zeros((len(tau_values),2))
+    max_error_array = np.zeros((len(tau_values),4))
+    avg_error_array = np.zeros((len(tau_values),4))
+    val_max_error_array = np.zeros((len(tau_values),4))
+    val_avg_error_array = np.zeros((len(tau_values),4))
      
     
     os.makedirs(dirname, exist_ok=True)
     with open(os.path.join(f'{dirname}', "error_stat.txt"), "w") as file:        
         for idx, tau in enumerate(tau_values):
             file.write(f'tau = {tau} and for max-error:\n') 
-            file.write(f'\t with non-strategic learner = {max_error[tau][False]}\n') 
-            file.write(f'\t with strategic learner = {max_error[tau][True]}\n')
+            file.write(f'\t with non-strategic learner (training) and non-strategic (test) = {max_error[tau][0]}\n') 
+            file.write(f'\t with non-strategic learner (training) and strategic (test) = {max_error[tau][1]}\n') 
+            file.write(f'\t with strategic learner (training) and strategic (test) = {max_error[tau][3]}\n') 
+
             
             file.write(f'tau = {tau} and for avg-error:\n') 
-            file.write(f'\t with non-strategic learner = {avg_error[tau][False]}\n') 
-            file.write(f'\t with strategic learner = {avg_error[tau][True]}\n')
+            file.write(f'\t with non-strategic learner (training) and non-strategic (test) = {avg_error[tau][0]}\n') 
+            file.write(f'\t with non-strategic learner (training) and strategic (test) = {avg_error[tau][1]}\n') 
+            file.write(f'\t with strategic learner (training) and strategic (test) = {avg_error[tau][3]}\n') 
 
-            max_error_array[idx, 0] = max_error[tau][False]
-            max_error_array[idx, 1] = max_error[tau][True]
-            avg_error_array[idx, 0] = avg_error[tau][False]
-            avg_error_array[idx, 1] = avg_error[tau][True]
+            max_error_array[idx, 0] = max_error[tau][0]
+            max_error_array[idx, 1] = max_error[tau][1]
+            max_error_array[idx, 3] = max_error[tau][3]
+
+            avg_error_array[idx, 0] = avg_error[tau][0]
+            avg_error_array[idx, 1] = avg_error[tau][1]
+            avg_error_array[idx, 3] = avg_error[tau][3]
+
+    with open(os.path.join(f'{dirname}', "val_error_stat.txt"), "w") as file:        
+        for idx, tau in enumerate(tau_values):
+            file.write(f'tau = {tau} and for val_max-error:\n') 
+            file.write(f'\t with non-strategic learner (training) and non-strategic (test) = {val_max_error[tau][0]}\n') 
+            file.write(f'\t with non-strategic learner (training) and strategic (test) = {val_max_error[tau][1]}\n') 
+            file.write(f'\t with strategic learner (training) and strategic (test) = {val_max_error[tau][3]}\n') 
+
+            
+            file.write(f'tau = {tau} and for val_avg-error:\n') 
+            file.write(f'\t with non-strategic learner (training) and non-strategic (test) = {val_avg_error[tau][0]}\n') 
+            file.write(f'\t with non-strategic learner (training) and strategic (test) = {val_avg_error[tau][1]}\n') 
+            file.write(f'\t with strategic learner (training) and strategic (test) = {val_avg_error[tau][3]}\n') 
+
+            val_max_error_array[idx, 0] = val_max_error[tau][0]
+            val_max_error_array[idx, 1] = val_max_error[tau][1]
+            val_max_error_array[idx, 3] = val_max_error[tau][3]
+
+            val_avg_error_array[idx, 0] = val_avg_error[tau][0]
+            val_avg_error_array[idx, 1] = val_avg_error[tau][1]
+            val_avg_error_array[idx, 3] = val_avg_error[tau][3]
     
      
     figures = []
-    figure_names = ['Strategic_vs_NonStrategic_MaxError', 'Strategic_vs_NonStrategic_PopError']
+    figure_names = ['MaxGroupError', 'AvgPopError', 'val_MaxGroupError', 'val_AvgPopError']
     plt.ion()
     # Average Pop error vs. Rounds
     figures.append(plt.figure())  # Creates figure and adds it to list of figures
-    learner_types = ['non-strategic', 'strategic']
-    for learner in [0, 1]:
+    learner_types = ['NS-NS', 'NS-S', '' , 'S-S']
+    for learner in [0, 1, 3]:
         # Plots the groups with appropriate label
         plt.plot(max_error_array[:, learner], label=learner_types[learner])
-
+ 
     plt.legend(loc='upper right')
-    plt.title(f'Strategic vs Non-Strategic Learner' + dataset_string)
+    plt.title(f'NS-NS vs NS-S vs S-S Learner' + dataset_string)
     
     tau_x_ticks = np.arange(0, len(max_error_array[:, learner]))
-    tau_x_labels = labels = [str(tau_values[i]) if i % 2 == 0 else '' for i in range(len(tau_values))]
+    tau_x_labels = [str(tau_values[i]) if i % 2 == 0 else '' for i in range(len(tau_values))]
     plt.xticks(tau_x_ticks, tau_x_labels)
     
     plt.xlabel('Manipulation Budget')
-    plt.ylabel(f'Max Population Error ({pop_error_type})')
+    plt.ylabel(f'Max Group Error ({pop_error_type})')
 
     if display_plots:
         plt.show()
     
     figures.append(plt.figure())
-    for learner in [0, 1]:
+    for learner in [0, 1, 3]:
         # Plots the groups with appropriate label
         plt.plot(avg_error_array[:, learner], label=learner_types[learner])
     
     plt.legend(loc='upper right')
     
    
-    plt.title(f'Strategic vs Non-Strategic Learner' + dataset_string)
+    plt.title(f'NS-NS vs NS-S vs S-S Learner' + dataset_string)
     plt.xticks(tau_x_ticks, tau_x_labels)
     plt.xlabel('Manipulation Budget')
     plt.ylabel(f'Average Population Error ({pop_error_type})')
 
     if display_plots:
         plt.show()
+
+    figures.append(plt.figure())  # Creates figure and adds it to list of figures
+    for learner in [0, 1, 3]:
+        # Plots the groups with appropriate label
+        plt.plot(val_max_error_array[:, learner], label=learner_types[learner])
+ 
+    plt.legend(loc='upper right')
+    plt.title(f'NS-NS vs NS-S vs S-S Learner' + dataset_string)
+    
+    tau_x_ticks = np.arange(0, len(val_max_error_array[:, learner]))
+    tau_x_labels = [str(tau_values[i]) if i % 2 == 0 else '' for i in range(len(tau_values))]
+    plt.xticks(tau_x_ticks, tau_x_labels)
+    
+    plt.xlabel('Manipulation Budget')
+    plt.ylabel(f'Max Group Error ({pop_error_type})')
+
+    if display_plots:
+        plt.show()
+    
+    figures.append(plt.figure())
+    for learner in [0, 1, 3]:
+        # Plots the groups with appropriate label
+        plt.plot(val_avg_error_array[:, learner], label=learner_types[learner])
+    
+    plt.legend(loc='upper right')
+    
+   
+    plt.title(f'NS-NS vs NS-S vs S-S Learner' + dataset_string)
+    plt.xticks(tau_x_ticks, tau_x_labels)
+    plt.xlabel('Manipulation Budget')
+    plt.ylabel(f'Average Population Error ({pop_error_type})')
+
+    if display_plots:
+        plt.show()
+
 
     save_plots_to_os(figures, figure_names, dirname)
     plt.close('all')
