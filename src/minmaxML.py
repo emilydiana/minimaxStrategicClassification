0601d6f0853d44dd871d74ccfd1b296f52cac44a
ediana <ediana@fac-mstudio-edi.tq.lan.local.cmu.edu>
2026-03-25 13:50:01 -0400

    social burden during testing

--- src/minmaxML.py
+++ src/minmaxML.py
@@ -556,7 +556,8 @@ def do_learning(X, y, numsteps, grouplabels, a=1, b=0.5, equal_error=False, scal
                         show_legend, error_type, data_name, model_string + f'\n Validation with test_size={test_size}',
                         val_agg_poperrs, val_agg_grouperrs, None,
                         pop_error_type, val_stacked_bonus_plots,
-                        dirname, tau, strategic_learner, validation=True, multi_group=True, curr_idx = curr_idx)
+                        dirname, tau, strategic_learner, validation=True, multi_group=True, curr_idx = curr_idx,
+                        agg_pop_social_burden = val_agg_pop_social_burden)
     else:  # Ensures that return doesn't fail when we aren't plotting
         stacked_bonus_plots = None
         if do_validation:
@@ -679,21 +680,21 @@ def compute_model_errors(modelhat, X, y, t, errors, error_type, penalty='none', 
     """
     Computes the error of the round-specific model and puts the errors for each sample in column t of `errors` in place
     """       
-    coef_ = None
-    y_pred = np.zeros(len(X))
-   
-    if isinstance(modelhat, LinearSVM):
-        coef_ = modelhat.coef_
-        y_pred = modelhat.decision_function(X)
-    else:
-        coef_ = modelhat.regressor.coef_
-        y_pred = modelhat.regressor.predict(X)
-    norm_coef = np.linalg.norm(coef_)
-    dist =  np.abs(y_pred) / norm_coef
-    move = (dist < tau_vector) & (y_pred < 0)
-    social_burden[t,:] = dist * move * y
-    
     if strategic_agent:    
+        coef_ = None
+        y_pred = np.zeros(len(X))
+   
+        if isinstance(modelhat, LinearSVM):
+            coef_ = modelhat.coef_
+            y_pred = modelhat.decision_function(X)
+        else:
+            coef_ = modelhat.regressor.coef_
+            y_pred = modelhat.regressor.predict(X)
+        norm_coef = np.linalg.norm(coef_)
+        dist =  np.abs(y_pred) / norm_coef
+        move = (dist < tau_vector) & (y_pred < 0)
+        social_burden[t,:] = dist * move * y
+    
         manipulation = np.outer(y_pred/norm_coef**2, coef_)    
         stratX = np.copy(X)
         stratX[move] = X[move] - manipulation[move]         
